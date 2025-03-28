747cad352535905768fed2c88c8ac1ac3bb1a308
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-03-29 00:00:59 +0530

    Update CombinationSumII.java

--- Backtracking/CombinationSumII.java
+++ Backtracking/CombinationSumII.java
@@ -48,7 +48,7 @@ class Solution {
      * At every level, we consider only one the first unique element and ignore its duplicates.
      * Base case: when `target == 0`.
      * Since, the recursion is inside a loop, we do not need `i == candidates.length` check.
-     * Eg. candidates = [1,1,1,2,2]
+     * Eg. candidates = [1,1,1,2,2], target = 4
      */
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 
