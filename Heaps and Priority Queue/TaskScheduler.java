08f5bd4f5e2cd8d8fd2c84175373ac2bff2f4ad5
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-03-31 14:20:46 +0530

    Update TaskScheduler.java

--- Heaps and Priority Queue/TaskScheduler.java
+++ Heaps and Priority Queue/TaskScheduler.java
@@ -1,21 +1,22 @@
 class Solution {
-    // refer codestorywithMIK + ChatGPT o3 for code
-    // BRUTE/BETT/OPTIMAL
+    // refer codestorywithMIK + ChatGPT o3 
+    // BRUTE/BETTER
     // can be done with Greedy as well.
-    // T: O(m + 26log26); m = tasks.length
+    // T: O(m); O(m) for `m` tasks + 26log26 for heap
     // S: O(26); 
     /*
      * 1. Intuition - If we pick the maximum frequency task first everytime, then overall time will reduce as CPU will remain less idle.
      * 2. So, we need to greedily choose the highest frequency task in every iteration.
-     * 3. In every cycle of cpu, we are processing for n+1 time.
+     * 3. In every cycle of cpu, we are processing for `n+1` time.
      * 4. Because, after processing a task for 1 unit, we have to wait for `n` to process it again. Hence, we process another task if available in that time.
      * 5. Code - a freq[] of size 26 for tasks, a maxHeap.
-     * Add all non-zero frequency to the heap initially.
+     * Add all non-zero frequencies to the heap initially.
      * 6. Loop over the heap and process the non-zero freq. elements.
-     * 7. In this, process the elements in a cycle. The cycle length = n+1. So process until the current cycle is complete. And add the non-zero frequency elements to a list.
-     * 8. After the cycle is complete, add the elements to the heap for processing in next cycle.
+     * 7. In this, process the elements in a cycle. The cycle length = n+1. So process until the current cycle is complete.
+     * Poll element, reduce its frequency and add the non-zero frequency elements to a list.
+     * 8. After the cycle is complete or heap is empty, add the elements from the list to the heap for processing in next cycle.
      * 9. If there are no tasks left, then heap is empty, so break.
-     * 10. Add the remaining cycle to the total time because it indicates the idle time.
+     * 10. Everytime, add the remaining cycle to the total time because it indicates the idle time.
      */
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
