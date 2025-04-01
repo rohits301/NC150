e2abe6cb0106173a4488b5018afc4bbdba725406
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-04-01 11:51:46 +0530

    Update DailyTemperatures.java

--- Stack/DailyTemperatures.java
+++ Stack/DailyTemperatures.java
@@ -29,16 +29,16 @@ class Solution {
     // S: O(n)
     /*
      * Intuition -
-     * I need a data structure to keep track of elements after current.
+     * 1. I need a data structure to keep track of elements after current.
      * So, a stack can help us do that.
-     * Every time, we need a warmer temp., so a value > current.
-     * That is, we want to keep track of next greater element (NGE).
-     * Also, we store indices in stack as a rule of thumb, because for arrays
+     * 2. Every time, we need a warmer temp., so a value > current.
+     * 3. That is, we want to keep track of next greater element (NGE).
+     * 4. Also, we store indices in stack as a rule of thumb, because for arrays
      * we can get values if we have indices.
-     * This is a monotonic decreasing stack, i.e, the elements from bottom to top
+     * 5. This is a monotonic decreasing stack, i.e, the elements from bottom to top
      * are in a non-increasing order or strictly decreasing order.
      * 
-     * NOTE: in thw `while` loop for `st.pop()`, we pop all values smaller and equal.
+     * NOTE: in the `while` loop for `st.pop()`, we pop all values smaller and equal.
      */
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
@@ -74,19 +74,22 @@ class Solution {
     // T: O(n)
     // S: O(1)
     /*
-     * Instead of going over all values in a brute force fasion, checking every
+     * 1. Instead of going over all values in a brute force fasion, checking every
      * single element, we can jump indices.
-     * e.g. if curr = 75 and curr + 1 = 72, ans for 72 = 2
+     * 2. e.g. if curr = 75 and curr + 1 = 72 and ans for 72 = 2
      * that means, ans for 75 is either index of (72) or index of (72) + 2.
-     * because, the number > 72 is after 2 places
-     * hence, if answer exists for 75, it will be after 2 places only.
+     * because, the number greater than 72 is after 2 places.
+     * 3. hence, if answer exists for 75, it will be after 2 places only.
      * So we can avoid the redundant checking.
-     * 
-     * This is an improvement on the BRUTE FORCE approach.
+     * 4. `ans[n-1] = 0` always, so start from `n-2`.
+     * 5. If no answer exists in the rest of the array, we update `j = n` to mark that for this position, no warmer day was found.
+     * 6. Jump `j` to the next warmer day, index of next warmer day = `j + ans[j]`.
+     * 7. This is an improvement over the BRUTE FORCE approach.
      */
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         int[] ans = new int[n];
+        // In java, all values in ans[] are by default 0.
 
         for (int i = n - 2; i >= 0; i--) {
             int j = i + 1; // next potentially warmer day
