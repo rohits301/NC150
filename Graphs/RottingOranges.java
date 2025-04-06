fc32fad55e3347466eb523d4639c93c3a539c798
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-04-06 22:23:03 +0530

    Update RottingOranges.java

--- Graphs/RottingOranges.java
+++ Graphs/RottingOranges.java
@@ -2,7 +2,23 @@ class Solution {
     // refer STRIVER
     // T: O(m*n); grid iteration: m*n + queue can have max m*n elements + m*n*4 (for all 4 directions) -> m*n + m*n*4
     // S: O(m*n); queue can have at max all m*n oranges for the case when all are rotten
-
+    /*
+     * 1. At every minute, oranges are rotten. Think, level order traversal in the graph.
+     * 2. After processing all the oranges at a level, we increment the time.
+     * 3. So, for BFS, we need a visited array, we can make a new arr[][] or use the grid itself 
+     * and mark rotten oranges with the given marker, i.e., `grid[i][j] = 2`.
+     * 4. Steps - 
+     * a) maintain count of initial fresh oranges
+     * b) add all the rotten oranges `(i,j)` to the queue.
+     * c) initialize time = -1, to adjust for the initial state of queue where we have some rotten oranges but haven't processed them yet.
+     * d) Traverse the queue level by level.
+     * e) In every iteration, `poll()` from queue
+     * f) iterate in the four directions to find neighbors and add them to the queue
+     * g) reduce the fresh count and mark the neighbors as visited while adding them
+     * h) At the end of each level, increment time
+     * i) if no fresh oranges remain, return the time taken, else -1.
+     * 5. Edge case: there is no fresh oranges initially, so return 0 as cannot rot.
+     */
     // Directions for 4-way adjacent cells (up, left, right, down)
     private static final int[][] directions = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
 
