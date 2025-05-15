13d7c28b083a3f6945f72159740aa8b4d7675cb2
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-05-15 23:53:44 +0530

    Update BestTimeToBuyAndSellStockIII.java

--- BestTimeToBuyAndSellStockIII.java
+++ BestTimeToBuyAndSellStockIII.java
@@ -36,3 +36,52 @@ class Solution {
         return profit;
     }
 }
+
+class Solution {
+    // refer STRIVER - DP 38.
+    // BETTER - MEMOIZATION
+    // TOP-DOWN
+    // T: O(n*2*3)
+    // S: O(n*2*3) + o(n)
+    /**
+     * 1. Memoizing the recursion requires maintaing an array of 
+     * size = (n+1)*2*3. Because, n states of `i`, 2 states of `buy` - 0,1, 3 states of `cap` - 0,1,2.
+     * 2. Initialize the 3D array with -1. Basically the 3D array is `n+1` 2D arrays of (2*3) dimension.
+     * 3. Return the result if already present.
+     */
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][3];
+        for(int[][] array2D: dp){
+            for(int[] array1D: array2D){
+                Arrays.fill(array1D, -1);
+            }
+        } 
+        return dfs(0, 1, 2, prices, dp);
+    }
+
+    private int dfs(int i, int buy, int cap, int[] prices, int[][][] dp){
+        if(i == prices.length){
+            return 0;
+        }
+        
+        if(cap == 0){
+            return 0;
+        }
+
+        if(dp[i][buy][cap] != -1){
+            return dp[i][buy][cap];
+        }
+
+        int profit = 0;
+        if(buy == 1){
+            profit = Math.max(-prices[i] + dfs(i+1, 0, cap, prices, dp),
+                                0 + dfs(i+1, 1, cap, prices, dp));
+        } else {
+            profit = Math.max(prices[i] + dfs(i+1, 1, cap - 1, prices, dp),
+                                0 + dfs(i+1, 0, cap, prices, dp));
+        }
+        return dp[i][buy][cap] = profit;
+    }
+}
+
