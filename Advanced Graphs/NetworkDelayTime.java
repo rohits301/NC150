e41ddbf4740ef4b82c7c0a0cc09988435f90f308
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-05-27 10:31:45 +0530

    Update NetworkDelayTime.java

--- Advanced Graphs/NetworkDelayTime.java
+++ Advanced Graphs/NetworkDelayTime.java
@@ -51,3 +51,73 @@ class Solution {
         return maxDelay;
     }
 }
+
+/**
+ * Striver Like code: using ArrayList for adjacency list and Pair class
+ */
+class Solution {
+    // single source shortest path - Dijikstra
+    // PQ approach - O(E*logV)
+    public static final int INF = Integer.MAX_VALUE;
+
+    public int networkDelayTime(int[][] times, int n, int k) {
+        // 1. construct graph
+        // Initialisation
+        List<List<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<Pair>());
+        }
+        // construction
+        for (int[] time : times) {
+            int u = time[0];
+            int v = time[1];
+            int wt = time[2];
+            adj.get(u).add(new Pair(wt, v));
+        }
+
+        // 2. Run dijikstra from source node `k`
+        int[] minTimes = new int[n + 1];
+        Arrays.fill(minTimes, INF);
+        minTimes[k] = 0; // source
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.distance - b.distance);
+        pq.offer(new Pair(0, k));
+
+        while (!pq.isEmpty()) {
+            Pair rp = pq.poll();
+            int distance = rp.distance;
+            int srcNode = rp.node;
+
+            for (Pair nbr : adj.get(srcNode)) {
+                int edgeWt = nbr.distance;
+                int adjNode = nbr.node;
+
+                if (distance + edgeWt < minTimes[adjNode]) {
+                    minTimes[adjNode] = distance + edgeWt;
+                    pq.offer(new Pair(minTimes[adjNode], adjNode));
+                }
+            }
+        }
+
+        int max = -1;
+        // REMEMBER -> START FROM 1 TO N
+        for (int i = 1; i <= n; i++) {
+            int val = minTimes[i];
+            if (val == INF) {
+                return -1;
+            }
+            max = Math.max(max, val);
+        }
+        return max;
+    }
+
+    private class Pair {
+        int distance;
+        int node;
+
+        Pair(int distance, int node) {
+            this.distance = distance;
+            this.node = node;
+        }
+    }
+}
